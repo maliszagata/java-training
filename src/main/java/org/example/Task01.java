231fdc2696cc846be0fcfe0441986d78c942f305
agata_m <agata.malisz2@gmail.com>
2022-02-27 23:25:30 +0100

    Task1 - solution

--- src/main/java/org/example/Task01.java
+++ src/main/java/org/example/Task01.java
@@ -2,6 +2,16 @@ package org.example;
 // https://adventofcode.com/2015/day/1
 public class Task01 {
     static int getCurrentFloor(String instructions) {
-        return 0;
+        int floor = 0;
+
+        for (int i = 0; i < instructions.length(); i++) {
+            if ("(".equals(String.valueOf(instructions.charAt(i)))) {
+                floor++;
+            } else if (")".equals(String.valueOf(instructions.charAt(i)))) {
+                floor--;
+            }
+        }
+
+        return floor;
     }
 }
