64136121d34e86fa5a980ffa16e79273abeecca6
agata_m <agata.malisz2@gmail.com>
2022-03-10 20:23:53 +0100

    All solutions
    (cherry picked from commit ee71e1d6aa32a9f512a74fed87aee65fb0a164d7)

--- src/main/java/org/example/Task04.java
+++ src/main/java/org/example/Task04.java
@@ -1,7 +1,18 @@
 package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
 // https://adventofcode.com/2018/day/1
 public class Task04 {
     static int getFrequency(String changes) {
-        return 0;
+
+        List<String> frequencyList = Arrays.asList(changes.split(", "));
+        int result = 0;
+        for (String s : frequencyList) {
+            result = result + Integer.parseInt(s);
+        }
+
+        return result;
     }
 }
