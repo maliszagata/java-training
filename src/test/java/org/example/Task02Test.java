ee71e1d6aa32a9f512a74fed87aee65fb0a164d7
agata_m <agata.malisz2@gmail.com>
2022-03-10 20:23:53 +0100

    All solutions

--- src/test/java/org/example/Task02Test.java
+++ src/test/java/org/example/Task02Test.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class Task02Test {
     @Test
-    void getPaperNeeded() {
+    public void getPaperNeeded() {
         assertEquals(Task02.getPaperNeeded("2x3x4"), 58);
         assertEquals(Task02.getPaperNeeded("1x1x10"), 43);
     }
